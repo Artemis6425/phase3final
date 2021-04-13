92b70065841ae2947164b3f40e5df4bfc20751bc
Artemis <43667548+Artemis6425@users.noreply.github.com>
2021-04-13 01:40:38 -0500

    Delete omniauth.rb
    private info

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Dotenv.load
-Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :twitch, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"]
-  provider :google_oauth2, '801119925631-vdafom612rle0lpkh6lqh7u01di3jegv.apps.googleusercontent.com', 'xdPl7o6uzVwyyWVbRvT2JIrj'
-end
