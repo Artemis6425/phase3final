86c7224b8777b91f9837bc8c8b10f8e0d31f25ce
Artemis6425 <aurorarose6425@gmail.com>
2021-04-21 12:22:06 -0500

    removed the presence_of validation, as it wasn't necessary and left behind

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :password
   validates_confirmation_of :password
   validates :name, :uniqueness => true
   has_many :runs
